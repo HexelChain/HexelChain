064218c9e0835995020f027707552e5323d73218
HexelChainn <manuelechavarria157@gmail.com>
2024-08-16 13:46:51 -0400

    feat: agent parser  (#210)
    * refactor: exclude runtime to outer file
    
    * feat: connection of web ui with aios
    
    * feat: connection of web ui with aios
    
    * bug: fresh exclude of cache
    
    * feat: unique process indexing
    
    * feat: rewire frontend to call specific agents concurrently
    
    * chore: centralized llm core
    
    * feat: prototyping next level llm core
    
    * feat: agent parser basic
    
    * feat: id recycling

---
+++ aios/hooks/parser.py
@@ -0,0 +1,31 @@
+from hexel.core.types import _obj, array, string, integer, _float
+from hexel.core.schema import CoreSchema
+from hexel.core import complete
+
+def useCompletion():
+    """
+    Wrapper for internal complete function
+    """
+
+    def _(
+        name: str,
+        message: str,
+        system_message: str | None = None,
+        temperature: float = 0.3,
+        max_tokens: float | None = None,
+        json: bool = False,
+        schema: CoreSchema | None = None
+    ):
+        return complete(
+            name,
+            message,
+            system_message,
+            temperature,
+            max_tokens,
+            json,
+            schema
+        )
+    
+    return _
+
+    
