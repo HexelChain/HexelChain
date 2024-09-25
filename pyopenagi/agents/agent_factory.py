32422a74d9fba1bb86514bdeacb0c334ffda6fe5
HexelChainn <manuelechavarria157@gmail.com>
2024-09-25 10:51:40 -0400

    Aios deploy (#255)
    * new branch
    
    * agent conflict changes

--- pyopenagi/agents/agent_factory.py
+++ pyopenagi/agents/agent_factory.py
@@ -27,7 +27,7 @@ class AgentFactory:
 
         self.agent_log_mode = agent_log_mode
 
-        self.manager = AgentManager('https://agenthub.hexel.foundation/')
+        self.manager = AgentManager('https://my.hexel.foundation/')
 
     def snake_to_camel(self, snake_str):
         components = snake_str.split('_')
